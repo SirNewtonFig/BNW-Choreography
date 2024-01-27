ad416c9b7f5b34440fb05d597488b4b6ce891fd3
SirNewtonFig <sirnewtonfig@gmail.com>
2024-01-27 12:53:48 -0400

    Use new model for selecting Dance step

--- asm/main.asm
+++ asm/main.asm
@@ -1,6 +1,10 @@
 hirom
 
-!free = $C23AC5     ; Former home of the Control effect handler, ripe for the picking
+!freeC2 = $C23AC5   ; Former home of the Control effect handler, ripe for the picking
+!freeE6 = $E68740   ; Freespace at end of "Pointers to Map Names" (E68400-E6877F)
+
+org $C24B5A
+  GetRandom:        ; Random Number Generator 2 (0 to 255)
 
 org $C21780
   AND #$FE          ; Always clear Dance status
@@ -16,37 +20,51 @@ org $C23C82         ; Reclaim space for Moogle Charm Dance wrapper
   pad $C23C8F
 
 org $C205B6         ; Modify how Dance step is determined
-  JSL PickType      ; X = index of Dance step
-  JSR $4B65         ; RNG (0..7)
-  CMP.l Thresholds,X  ; Compare number with threshold
-  BCS +             ; Use the common step if above
-  INC $EE           ; Use the uncommon step if below
-+ LDX $EE
+  JSR $4B5A         ; A = 0..255 (unchanged from vanilla, included for context)
+  JSL StepHelper    ; Sets $EE to the index of the desired Dance step
+  LDX $EE
   LDA $CFFE80,X     ; Get attack # for the Dance step used
   PLX
   RTS
 
-; Data
-Thresholds:
-  db $05            ; Transition steps: choose A if RNG(0..19) >= 5 (75%), else B
-  db $08            ; Sustain steps: choose A if RNG(0..19) >= 8 (60%), else B
-
 padbyte $FF
-pad $C205D1         ; 2 bytes reclaimed, wowee
+pad $C205D1          ; 7 bytes reclaimed!
 
-org !free
-PickType:
-  LDA $3A6F
-  LDX $11E2
-  CMP $ED8E5B,X     ; Check if background is same as dance
-  BNE +             ; Branch if not
-  rep 2 : INC $EE   ; Use lower half of step list for sustain steps
-  LDX #$01          ; Use sustain probabilities
-  BRA ++
-+ LDX #$00          ; Use transition probabilities
-++ LDA #$14
-+ RTL
+org !freeE6
+StepHelper:          ; Select step based on RNG value
+  CMP #$60
+  BCS .common        ; ..do common   if >= #$60 (10/16)
+  CMP #$10
+  BCS .uncommon      ; ..do uncommon if >= #$10 (5/16)
+.rare
+  LDA #$03           ; ..do rare     if < #$10 (1/16)
+  BRA .end
+.uncommon
+  LDA #$02
+  BRA .end
+.common              ; Select which common step to use based on background
+  JSR BGHelper       ; ..C = 1 if background matches chosen Dance
+  TDC                ; ..A = 0
+  ADC #$00           ; ..A = 0 if shifting, 1 if repeating
+.end
+  CLC
+  ADC $EE            ; Add step index to Dance table pointer
+  STA $EE            ; Store it
+  RTL
 
+BGHelper:
+  LDA $3A6F          ; Load selected Dance
+  LDX $11E2          ; Load current background
+  CMP $ED8E5B,X      ; Check if Dance for current BG matches selected Dance
+  BEQ .repeat        ;
+  CLC                ; ..clear carry  if shifting
+  RTS                ;
+.repeat              ;
+  SEC                ; ..set carry if repeating
+  RTS
+
+
+org !freeC2
 StumbleCheck:
   PHA
   LDA $3C59,Y       ; Relic Effects 4
@@ -58,8 +76,53 @@ StumbleCheck:
 ++ PLA
   RTS
 
-padbyte $FF         ; Reclaim (most of) the rest of the routine
-pad $C23B1B         ; After this, some of the Control code miss condition is still required
+org $CFFE80 ; Reorganize Dance Step -> Attack Number table
 
-incsrc "dance-tables.asm"
-incsrc "dance-descriptions.asm"
+; Wind Song
+db $66 ; Sun Bath   ; Shift
+db $65 ; Wind Slash ; Repeat
+db $67 ; Razor Leaf ; Uncommon
+db $75 ; Cockatrice ; Rare
+
+; Forest Suite
+db $67 ; Razor Leaf ; Shift
+db $68 ; Harvester  ; Repeat
+db $6B ; Elf Fire   ; Uncommon
+db $7A ; Raccoon    ; Rare
+
+; Desert Aria
+db $6E ; Mirage     ; Shift
+db $66 ; Sun Bath   ; Repeat
+db $69 ; Sand Storm ; Uncommon
+db $77 ; Meerkat    ; Rare
+
+; Love Sonata
+db $6B ; Elf Fire   ; Shift
+db $6C ; Bedevil    ; Repeat
+db $6A ; Moonlight  ; Uncommon
+db $78 ; Tapir      ; Rare
+
+; Earth Blues
+db $ED ; Landslide  ; Shift
+db $6D ; Avalanche  ; Repeat
+db $66 ; Sun Bath   ; Uncommon
+db $79 ; Wild Boars ; Rare
+
+; Water Rondo
+db $6F ; El Nino    ; Shift
+db $70 ; Plasma     ; Repeat
+db $74 ; Surge      ; Uncommon
+db $7B ; Toxic Frog ; Rare
+
+; Dusk Requiem
+db $6A ; Moonlight  ; Shift
+db $71 ; Snare      ; Repeat
+db $72 ; Cave In    ; Uncommon
+db $76 ; Wombat     ; Rare
+
+; Snowman Jazz
+db $73 ; Blizzard   ; Shift
+db $74 ; Surge      ; Repeat
+db $6E ; Mirage     ; Uncommon
+db $7C ; Ice Rabbit ; Rare
+
